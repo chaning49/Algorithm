47d33a3a8d16cdd2ec79803843f83ee9e2e4e2c3
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-10-13 01:52:27 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 196 ms, Memory: 16360 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,51 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String s = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         StringBuilder temp = new StringBuilder();
-
         boolean isTag = false;
 
-        // 1. < 문자가 등장하기 전까지는 뒤집어서 이어붙인다.
-        // 2. < 문자가 등장하면 > 문자까지 태그이므로 그대로 이어붙인다.
-        // 3. > 문자 이후로 다시 < 문자를 만날 때까지는 뒤집어서 이어붙인다.
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '<') {
-                while (!temp.toString().equals("")) {
-                    sb.append(temp.reverse());
-                    temp.setLength(0);
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '<') {
+                answer.append(temp.reverse());
+                temp.setLength(0);  // StringBuilder 초기화
                 isTag = true;
-            } else if (str.charAt(i) == '>') {
-                isTag = false;
-                sb.append(temp).append(">");
-                temp.setLength(0);
-                continue;
             }
 
             if (isTag) {
-                temp.append(str.charAt(i));
+                temp.append(c);
+            } else if (c == ' ') {
+                answer.append(temp.reverse()).append(c);
+                temp.setLength(0);
             } else {
-                if (str.charAt(i) == ' ') {
-                    sb.append(temp.reverse()).append(" ");
-                    temp.setLength(0);
-                } else {
-                    temp.append(str.charAt(i));
-                }
+                temp.append(c);
+            }
+
+            if (c == '>') {
+                answer.append(temp);
+                temp.setLength(0);
+                isTag = false;
             }
         }
 
-        if (temp.length() != 0) {
-            sb.append(temp.reverse());
-        }
+        // 남아있는 단어 처리
+        answer.append(temp.reverse());
 
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }
