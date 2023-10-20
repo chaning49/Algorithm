36a26db73639de93363a8142fefb3f9af4774d63
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-10-20 10:18:45 +0900

    [level 2] Title: 하노이의 탑, Time: 6.55 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 class Solution {
+    static List<int[]> path;
+    
     public int[][] solution(int n) {
-        int[][] answer = {};
-        return answer;
+        path = new ArrayList<>();
+        hanoi(n, 1, 3, 2);
+        
+        return path.toArray(new int[path.size()][2]);
+    }
+    
+    public void hanoi(int n, int from, int to, int via) {
+        if (n == 1) {
+            path.add(move(from, to));
+            return;
+        }
+        
+        hanoi(n - 1, from, via, to);
+        path.add(move(from, to));
+        hanoi(n - 1, via, to, from);
+    }
+    
+    private int[] move(int from, int to) {
+        return new int[]{from, to};
     }
 }
