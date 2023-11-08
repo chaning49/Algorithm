c5d39605353025fdf9c74ec8575c2b198391a4e9
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-11-08 19:02:40 +0900

    [Bronze II] Title: 트럭 주차, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -11,43 +11,27 @@ public class Main {
         int c = Integer.parseInt(inputs[2]);
 
         // 시간은 1부터 100
-        int[] truck = new int[101];
-        int[] inArr = new int[101];
-        int[] outArr = new int[101];
+        int[] cnt = new int[101];
 
-        // 같은 시간이 in or out인 경우도 생각해줘야 한다.
-        // 입력된 입차, 출차 값을 times의 인덱스에 in, out으로 표시한다.
+        // 입차가 일어난 시간을 기록해준다.
         for (int i = 0; i < 3; i++) {
             String[] input = br.readLine().split(" ");
             int in = Integer.parseInt(input[0]);
             int out = Integer.parseInt(input[1]);
-            inArr[in]++;
-            outArr[out]++;
+            for (int j = in; j < out; j++) {
+                cnt[j]++;
+            }
         }
-        
+
         int ans = 0;
         for (int i = 1; i < 101; i++) {
-            // 입차, 출차가 없으면 이전 값을 그대로 가져온다.
-            if (inArr[i] == 0 && outArr[i] == 0) {
-                truck[i] = truck[i - 1];
-            } else { // 하나라도 입차, 출차가 있으면 입차는 더하고, 출차는 빼준다.
-                truck[i] = truck[i - 1] + inArr[i] - outArr[i];
-            }
-            
-            // 트럭 수에 따른 요금 계산 
-            switch (truck[i]) {
-                case 1:
-                    ans += a;
-                    break;
-                case 2:
-                    ans += b * 2;
-                    break;
-                case 3:
-                    ans += c * 3;
-                    break;
+            if (cnt[i] != 0) {
+                if (cnt[i] == 1) ans += a;
+                else if (cnt[i] == 2) ans += b * 2;
+                else if (cnt[i] == 3) ans += c * 3;
             }
         }
-        
+
         System.out.println(ans);
     }
 }
