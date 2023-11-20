5a234f440e6003f8e7f7d5574d36c30d5689d2fb
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-11-21 00:30:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 74.55 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        // 총 횟수
         int answer = 0;
         
         // 큐 선언
@@ -27,8 +28,8 @@ class Solution {
         
         // target과 값이 같아질 때까지 반복하면서 총합이 더 큰쪽에서 값을 pop하고 작은 쪽으로 push한다.
         // 만약 한쪽 큐가 비는 상황이 생기면 정답이 없는 것이므로 -1
-        // 최대로 while문을 순회하는 횟수는 queue의 2배이다. (queue1과 queue2의 크기가 같기 때문에 최악의 경우를 봐도 원소를 한쪽으로 모두 옮기는 작업이 총 2회이다.)
-        while (answer < 4 * queue1.length) {
+        // 최대로 while문을 순회하는 횟수는 queue의 2배이다. (queue1과 queue2의 크기가 같기 때문에 최악의 경우를 봐도 원소를 한쪽으로 모두 옮기는 작업이 총 2회이다.) -> 하지만 실제로 *특정 테스트 케이스에서 2 * queue1.length를 넘어가는 경우가 생긴다. 총 반복 횟수가 2 *queue.length < 반복횟수 < 3 * queue1.length인 경우가 있다. 그래서 while문의 반복 횟수는 3 * queue1.length로 해주어야 한다.
+        while (answer < 3 * queue1.length) {
             if (sumOne == target) { // 정답인 경우 바로 return
                 return answer;
             }
