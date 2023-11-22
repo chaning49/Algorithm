2c54857b53bc3fa54527c2124676c5319bbd678a
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-11-22 23:31:04 +0900

    [level 2] Title: 귤 고르기, Time: 51.88 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476#qna) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: 94.5 MB, 시간: 51.88 ms
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2023년 11월 3일 23:24:56
+2023년 11월 3일 23:31:2
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        
-        // Map에 귤의 크기, 개수를 저장
-        Map<Integer, Integer> gyul = new HashMap<>();
-        for (int t : tangerine) {
-            if (!gyul.containsKey(t)) {
-                gyul.put(t, 1);
-            } else {
-                gyul.put(t, gyul.get(t) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int key : map.keySet()){
+            pq.add(map.get(key));
+        }
+
+        int cnt = 0, sum = 0;
+        while(!pq.isEmpty()){
+            int cur = pq.poll();
+
+            if(sum < k){
+                sum += cur;
+                cnt++;
+            }
+
+            if(sum >= k){
+                break;
             }
         }
-        
-        // 귤의 개수만을 저장 후 sort
-        int[] sortedTan = new int[gyul.size()];
-        int i = 0;
-        for (int g : gyul.keySet()) {
-            sortedTan[i] = gyul.get(g);
-            i++;
-        }
-        Arrays.sort(sortedTan);
-        
-        // 가장 개수가 많은 귤을 기준으로 k개를 없앨 수 있도록 제거
-        i = sortedTan.length - 1;
-        while (k > 0) {
-            k -= sortedTan[i--];
-            answer++;
-        }
-        
-        return answer;
+
+        return cnt;
     }
 }
