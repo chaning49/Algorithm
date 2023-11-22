826b643cdf2c023a86364a693f952fc7c8c41045
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-11-22 22:54:19 +0900

    [level 2] Title: 귤 고르기, Time: 58.57 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        // Map에 귤의 크기, 개수를 저장
+        Map<Integer, Long> gyul = new HashMap<>();
+        for (int t : tangerine) {
+            if (!gyul.containsKey(t)) {
+                gyul.put(t, 1L);
+            } else {
+                gyul.put(t, gyul.get(t) + 1L);
+            }
+        }
+        
+        // 귤의 개수만을 저장 후 sort
+        long[] sortedTan = new long[gyul.size()];
+        int i = 0;
+        for (int g : gyul.keySet()) {
+            sortedTan[i] = gyul.get(g);
+            i++;
+        }
+        Arrays.sort(sortedTan);
+        
+        // 가장 개수가 많은 귤을 기준으로 k개를 없앨 수 있도록 제거
+        i = sortedTan.length - 1;
+        while (k > 0) {
+            k -= sortedTan[i];
+            i--;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
