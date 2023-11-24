0441cd279b8af8b7c816c51cce92a2fa13631703
chaning49 <79316402+chaning49@users.noreply.github.com>
2023-11-24 22:13:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 0.22 ms
+메모리: 72.5 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 22:11:8
+2023년 11월 5일 22:13:52
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,17 @@
 class Solution {
     public long solution(int n) {
         // 규칙: a[i] = a[i - 2] + a[i - 1];
-        long answer = 0;
-        
         // 피보나치 저장용 배열 선언
-        int[] fibo = new int[n + 4];
-        fibo[1] = 1;
-        fibo[2] = 2;
-        fibo[3] = 3;
+        long[] fibo = new long[n + 4];
+        fibo[1] = 1l;
+        fibo[2] = 2l;
+        fibo[3] = 3l;
         
         // 값을 구할때마다 모듈러 연산
         for (int i = 4; i <= n; i++) {
-            fibo[i] = ((fibo[i - 2] % 1234567) + (fibo[i - 1] % 1234567)) % 1234567;
+            fibo[i] = (fibo[i - 2] + fibo[i - 1]) % 1234567;
         }
         
-        answer = fibo[n] % 1234567;
-        
-        return answer;
+        return fibo[n];
     }
 }
