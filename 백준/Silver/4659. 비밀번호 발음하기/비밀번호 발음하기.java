b1dbf152dbff8b56592de96ad2959fb635c1a244
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-03-22 00:07:58 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 112 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -13,36 +13,40 @@ public class Main {
         
         while (true) {
             str = br.readLine();
-
+            
+            // 종료 조건
             if (str.equals("end")) {
                 break;
             }
 
             isAcceptable = false;
-            prev = '0';
-            int vowels = 0;
-            int consonants = 0;
+            prev = '0'; // 이전 문자 초기화
+            int consonants = 0; // 자음의 개수
+            int vowels = 0; // 모음의 개수
 
             for (char c : str.toCharArray()) {
-                if (isVowel(c)) {
+                if (isVowel(c)) { // 모음인 경우
                     isAcceptable = true;
                     vowels++;
                     consonants = 0;
-                } else {
+                } else { // 자음인 경우
                     vowels = 0;
                     consonants++;
                 }
-
+                
+                // 자음 또는 모음이 3개 연속인 경우 -> not acceptable
                 if (consonants == 3 || vowels == 3) {
                     isAcceptable = false;
                     break;
                 }
-
+                
+                // 이전 문자와 현재 문자가 같고, ee 또는 oo가 아닌 경우 -> not acceptable
                 if (c == prev && c != 'e' && c!= 'o') {
                     isAcceptable = false;
                     break;
                 }
-
+                
+                // 통과하면 현재 문자를 이전 문자로 변경
                 prev = c;
             }
             if (isAcceptable) sb.append("<").append(str).append(ACCEPTABLE);
@@ -52,6 +56,7 @@ public class Main {
         System.out.println(sb);
     }
     
+    // 모음인지 판별
     public static boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
