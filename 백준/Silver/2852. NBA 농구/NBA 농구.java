3a31998f416aa1543aba640991b389aa9271b8b5
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-03-22 01:32:47 +0900

    [Silver III] Title: NBA 농구, Time: 152 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.java
+++ 백준/Silver/2852. NBA 농구/NBA 농구.java
@@ -15,23 +15,29 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            team = st.nextToken();
-            sec = st.nextToken();
-
+            team = st.nextToken(); // 팀 번호
+            sec = st.nextToken(); // 시간
+            
+            // 점수가 더 높은 팀이 생기면 현재 시간 기준으로 이전 시간과 비교하여 계산
             if (one > two) calcOne(sec);
             else if (two > one) calcTwo(sec);
-
+            
+            // 득점한 팀 번호 증가
             if (team.equals("1")) one++;
             else two++;
-
+            
+            // 이전 시간 저장
             prev = sec;
         }
+        // 경기 종료 시간까지 이기고 있는 팀 시간 계산
         if (one > two) calcOne("48:00");
         else if (two > one) calcTwo("48:00");
+        
         System.out.println(printTime(oneSum));
         System.out.println(printTime(twoSum));
     }
-
+    
+    // 이기고 있는 팀의 점수 계산
     public static void calcOne(String sec) {
         oneSum += (changeToSec(sec) - changeToSec(prev));
     }
@@ -39,17 +45,20 @@ public class Main {
     public static void calcTwo(String sec) {
         twoSum += (changeToSec(sec) - changeToSec(prev));
     }
-
-    public static int changeToSec(String sec) {
+    
+    // "01:30" 형태의 문자열을 90과 같은 초 단위로 변경 
+    private static int changeToSec(String sec) {
         String[] input = sec.split(":");
         return Integer.parseInt(input[0]) * 60 + Integer.parseInt(input[1]);
     }
-
+    
+    // 90과 같은 초 단위를 "01:30" 같은 문자열로 변경
     public static String printTime(int sum) {
         return appendTime(sum / 60) + ":" + appendTime(sum % 60);
     }
-
-    public static StringBuilder appendTime(int time) {
+    
+    // 시간의 자릿수 계산
+    private static StringBuilder appendTime(int time) {
         return (time >= 10) ? new StringBuilder().append(time) : new StringBuilder().append("0").append(time);
     }
 }
