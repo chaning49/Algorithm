6c428690a94c629d9ef8916e378bac22af8d3d30
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-04-17 00:55:51 +0900

    [level 1] Title: 최소직사각형, Time: 1.46 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 1.53 ms
+메모리: 87 MB, 시간: 1.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 00:44:47
+2024년 04월 17일 00:55:50
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,24 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        int w = 0, h = 0;
+        int mxw = 0, mxh = 0;
         
-        // sizes를 순회하면서 가로와 세로의 최대값 찾기(가로를 더 큰 값을 가지도록 설정)
+        // 회전하면서 가로와 세로의 최대값 찾기(가로를 더 큰 값을 가지도록 설정)
         for (int[] size : sizes) {
-            int wTemp = size[0];
-            int hTemp = size[1];
-            if (wTemp < hTemp) { // swap
-                int temp = wTemp;
-                wTemp = hTemp;
-                hTemp = temp;
-            }
-            if (w < wTemp) w = wTemp;
-            if (h < hTemp) h = hTemp;
+            int w = Math.max(size[0], size[1]);
+            int h = Math.min(size[0], size[1]);
+            mxw = Math.max(mxw, w);
+            mxh = Math.max(mxh, h);
         }
         
-        answer = w * h;
-        
-        return answer;
+        return mxw * mxh;
     }
 }
