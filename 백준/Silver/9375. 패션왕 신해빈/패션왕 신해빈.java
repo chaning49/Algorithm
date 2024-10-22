c13f43f99e16518118bab4157aa107f58b59cf81
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-10-22 22:58:51 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,45 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*
-        * boj 9375 패션왕 신해빈
-        * 입력: 테스트 케이스, 의상의 수와 종류
-        * 출력: 섞어서 입을 수 있는 경우의 수
-        * 착용할 아이템의 종류가 몇 개인지만 저장하면 된다. 예시를 보면 쉽게 알 수 있다.
-        * head 2개, eye 1개 -> 경우의 수는 곱셈이다. 이 때, 착용하지 않는 경우까지 고려하면 각 아이템에 +1씩 해준 후 곱한 다음 둘다 착용하지 않는 경우를 -1
-        * */
+        // 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-
-        // 테스트케이스 만큼 반복
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
+        // 총 n회의 시행
+        for (int i = 0; i < n; i++) {
+            int tc = Integer.parseInt(br.readLine());
+            // 자료구조 뭐 쓰지? map?
             Map<String, Integer> map = new HashMap<>();
-            for (int j = 0; j < n; j++) {
-                String[] inputs = br.readLine().split(" ");
-                if (!map.containsKey(inputs[1])) {
-                    map.put(inputs[1], 1);
-                } else {
-                    map.put(inputs[1], map.get(inputs[1]) + 1);
-                }
+            for (int j = 0; j < tc; j++) {
+                String[] input = br.readLine().split(" ");
+                map.put(input[1], map.getOrDefault(input[1], 0) + 1);
             }
 
             long ans = 1;
-            for (String key : map.keySet()) {
-                ans *= (map.get(key) + 1);
+            for (String str : map.keySet()) {
+                ans *= (map.get(str) + 1);
             }
-            ans--;
-
-            sb.append(ans).append("\n");
+            System.out.println(ans - 1);
         }
-
-        System.out.print(sb);
     }
 }
