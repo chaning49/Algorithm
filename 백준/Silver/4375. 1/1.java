c201128779cfaba7d55b2f95e12fb6e8478f855c
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-10-25 11:52:21 +0900

    [Silver III] Title: 1, Time: 108 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,32 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int n;
     public static void main(String[] args) throws IOException {
-        /*
-        * boj 4375 1
-        * 입력: n이 주어진다.(2 또는 5로 나누어 떨어지지 않음)
-        * 출력: 각 자릿수가 모두 1로만 이루어진 n의 배수 중 가장 작은 수의 자리수를 출력
-        * */
+        // 모듈러 연산의 특징을 이해하여야 한다.
+        // (a + b) % c = ((a % c) + (b % c)) % c
+        // (a * b) % c = ((a % c) * (b % c)) % c
+
+        // 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s;
-        StringBuilder sb = new StringBuilder();
-        while ((s = br.readLine()) != null) {
-            int n = Integer.parseInt(s);
-            int cnt = 1;
-            int ans = 1;
+        String input;
+        while ((input = br.readLine()) != null) {
+            n = Integer.parseInt(input);
+
+            // 1, 11, 111, ... 을 만들 cnt와 자리수를 저장할 ret 선언
+            long cnt = 1;
+            long ret = 1;
+
             while (true) {
-                if (cnt % n == 0) {
-                    sb.append(ans).append("\n");
+                if (cnt % n == 0) { // 1, 11, 111, ...  중에서 n으로 나누어 떨어지는, 즉 배수인 경우 stop
+                    System.out.println(ret);
                     break;
                 } else {
-                    cnt = (cnt * 10) + 1;
-                    cnt %= n;
-                    ans++;
+                    cnt = (cnt * 10) + 1; // 1, 11, 111을 만들기 위한 연산
+                    cnt %= n; // long의 범위를 벗어나는 경우를 고려하여 곱셈마다 모듈러 연산
+                    ret++;
                 }
             }
         }
-        System.out.println(sb);
     }
 }
