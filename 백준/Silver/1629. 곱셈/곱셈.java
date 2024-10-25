20fc67a7ed27bda9b49b4da8ee68442fb6ff1c38
chaning49 <79316402+chaning49@users.noreply.github.com>
2024-10-25 11:17:11 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,37 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static long c;
     public static void main(String[] args) throws IOException {
-        /*
-        * boj 1629 곱셈
-        * 입력: a, b, c는 최대 21억
-        * 출력: a를 b번 곱한 값을 c로 나눈 나머지
-        * 범위 때문에 a를 b번 곱하는 경우, overflow가 발생하므로 각 연산마다 c로 모듈러 연산을 해주는 동시에 재귀적 호출을 통해 O(logN)의 시간을 갖도록 한다.
-        * */
+        // 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
         c = Long.parseLong(st.nextToken());
 
+        // 재귀함수 돌리기
         System.out.println(go(a, b));
     }
 
-    private static long go(long a, long b) {
-        // 지수가 1인 경우
-        if (b == 1) {
-            return a % c;
-        }
-        long temp = go(a, b / 2);
-        temp = (temp * temp) % c;
-        if (b % 2 == 1) { // 지수가 홀수인 경우
-            temp = (temp * a) % c;
-        }
-        return temp;
+
+    // 분할 정복 Algorithm
+    public static long go(long a, long b) {
+        // 기저사례, 지수가 1이 되는 경우
+        if (b == 1) return a % c;
+
+        // 재귀 호출로 절반 계산
+        long ret = go(a, b / 2);
+
+        // 계산된 값을 제곱하여 모듈러 연산
+        ret = (ret * ret) % c;
+
+        // 지수가 홀수인 경우 a를 추가로 곱해주어야 한다.
+        if (b % 2 == 1) ret = (ret * a) % c;
+
+        return ret;
     }
 }
